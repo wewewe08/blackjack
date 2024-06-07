ac6e3cfef8c3b62359d5b2b5bebc01e2fe775c12
yvonne <yvonnehuang0810@gmail.com>
2024-06-07 01:19:27 -0400

    new card button

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ let isAlive = true
 let message = ""
 
 let startButtonEl = document.getElementById("start-btn");
+let cardsButtonEl = document.getElementById("draw-btn")
+
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-function startGame() {
+function StartGame() {
+    cardsEl.textContent += " " + firstCard + " " + secondCard;
     sumEl.textContent += " " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -23,4 +27,9 @@ function startGame() {
     messageEl.textContent = message;
 }
 
-startButtonEl.addEventListener("click", startGame);
+function DrawNewCard() {
+    console.log("Drawing a new card from the deck!");
+}
+
+startButtonEl.addEventListener("click", StartGame);
+cardsButtonEl.addEventListener("click", DrawNewCard);
